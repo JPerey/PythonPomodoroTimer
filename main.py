e0b57c90e5512eda36a721b73b3cae87461b5a0b
Jam Jam James Perey <jamjam.perey@gmail.com>
2022-10-25 00:40:20 -0700

    INITIAL PUSH: initial files

---
+++ main.py
@@ -0,0 +1,37 @@
+from tkinter import *
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Pomodoro = Tomato")
+background = PhotoImage(file="tomato.png")
+
+canvas = Canvas(width=200, height=224)
+canvas.create_image(103, 112, image=background)
+canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
+canvas.pack(padx=100, pady=50)
+
+
+
+
+
+
+
+
+
+window.mainloop()
